52c29e8fab10c0ff4cee3fc6a931bfcdf83b0459
André R. B <bronca.andre@gmail.com>
2022-08-31 20:04:09 -0300

    criação de módulo, jar, execução do jar

---
+++ chapter03/bj9f/src/jdojo.intro/com/jdojo/intro/Welcome.java
@@ -0,0 +1,33 @@
+package com.jdojo.intro;
+//java9_eb0044/chapter03/bj9f/src/jdojo.intro/com/jdojo/intro 
+/*
+ * Nome completo da classe: com.jdojo.intro.Welcome.java
+ * Nome simples: Welcome
+ */
+
+import java.util.*;
+
+public class Welcome {
+  public static void main(String[] args){
+    System.out.println("Welcome to Java 9!");
+  }
+
+  //int é do tipo primitivo
+  //void: não retorna nada ao chamador
+  //outra forma de declarar um parametro que recebe vários tipos int
+  //um array pode vir com um ou vários números do tipo int
+  void methodExample02(int[] nums){
+  
+  }
+
+  //mesma coisa que o método anterior
+  void methodExample03(int nums[]){
+  
+  }
+
+  //Recebendo texto, o tipo é String. String é uma classe (objeto)
+  void methodExample04(String[] nomes){
+  
+  }
+
+};	//optional ';', deixar apenas nessa classe para fins de conhecimento
---
+++ chapter03/bj9f/src/jdojo.intro/module-info.java
@@ -0,0 +1,6 @@
+// module-info.java
+module jdojo.intro {
+  // módulo padrão: não precisa adicionar, o compilador faz automaticamente.
+  //requires java.base;
+
+}
