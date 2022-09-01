ed0cbc4bdc009a4f2f6e7b242f5395a23c5867e0
André R. B <bronca.andre@gmail.com>
2022-09-01 19:26:57 -0300

    criação de um projeto com módulo no netbeans 13

---
+++ chapter03/Java9Fundamentals/src/jdojo.intro/classes/com/jdojo/intro/Welcome.java
@@ -0,0 +1,11 @@
+package com.jdojo.intro;
+
+/**
+ *
+ * @author andre
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java 9 in Netbeans 13");
+    }
+}
